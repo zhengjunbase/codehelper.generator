9b1ab9f60fdfd3c8a42a332fe7758a278ce63402
gejun123456 <gejunxxx@163.com>
2016-12-06 08:57:44 +0800

    add generate xml as some.

---
+++ src/main/java/com/ccnode/codegenerator/view/GenerateMethodXmlAction.java
@@ -0,0 +1,140 @@
+package com.ccnode.codegenerator.view;
+
+import com.intellij.codeInsight.intention.PsiElementBaseIntentionAction;
+import com.intellij.ide.util.PropertiesComponent;
+import com.intellij.openapi.command.CommandProcessor;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.module.Module;
+import com.intellij.openapi.module.ModuleUtilCore;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.roots.ModuleRootManager;
+import com.intellij.openapi.ui.Messages;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.psi.*;
+import com.intellij.psi.util.PsiTreeUtil;
+import com.intellij.testIntegration.TestFramework;
+import com.intellij.testIntegration.createTest.CreateTestDialog;
+import com.intellij.testIntegration.createTest.TestGenerator;
+import com.intellij.testIntegration.createTest.TestGenerators;
+import com.intellij.util.IncorrectOperationException;
+import org.jetbrains.annotations.Nls;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.jps.model.java.JavaSourceRootType;
+
+import java.util.HashSet;
+
+/**
+ * Created by bruce.ge on 2016/12/5.
+ */
+public class GenerateMethodXmlAction extends PsiElementBaseIntentionAction {
+
+    private static final String CREATE_TEST_IN_THE_SAME_ROOT = "create.test.in.the.same.root";
+    public static final String GENERATE_DAOXML = "generate daoxml";
+
+    @Override
+    public void invoke(@NotNull Project project, Editor editor, @NotNull PsiElement element) throws IncorrectOperationException {
+        Module srcModule = ModuleUtilCore.findModuleForPsiElement(element);
+        PsiClass srcClass = getContainingClass(element);
+        if (srcClass == null) return;
+        PsiDirectory srcDir = element.getContainingFile().getContainingDirectory();
+        PsiPackage srcPackage = JavaDirectoryService.getInstance().getPackage(srcDir);
+        PropertiesComponent propertiesComponent = PropertiesComponent.getInstance();
+        HashSet<VirtualFile> testFolder = new HashSet<>();
+        checkForTestRoots(srcModule, testFolder);
+        if (testFolder.isEmpty() && !propertiesComponent.getBoolean(CREATE_TEST_IN_THE_SAME_ROOT, false)) {
+            if (Messages.showOkCancelDialog(project, "Create test in the same source root?", "No Test Roots Found",
+                    Messages.getQuestionIcon()) != Messages.OK) {
+                return;
+            }
+            propertiesComponent.setValue(CREATE_TEST_IN_THE_SAME_ROOT, String.valueOf(true));
+        }
+        CreateTestDialog d = createTestDialog(project, srcModule, srcClass, srcPackage);
+        if (!d.showAndGet()) {
+            return;
+        }
+        CommandProcessor.getInstance().executeCommand(project, new Runnable() {
+            @Override
+            public void run() {
+                TestFramework framework = d.getSelectedTestFrameworkDescriptor();
+                TestGenerator testGenerator = TestGenerators.INSTANCE.forLanguage(framework.getLanguage());
+                testGenerator.generateTest(project, d);
+            }
+        }, GENERATE_DAOXML, this);
+    }
+
+    private CreateTestDialog createTestDialog(Project project, Module srcModule, PsiClass srcClass, PsiPackage srcPackage) {
+        return new CreateTestDialog(project, GENERATE_DAOXML, srcClass, srcPackage, srcModule);
+    }
+
+    private static void checkForTestRoots(Module srcModule, HashSet<VirtualFile> testFolder) {
+        checkForTestRoots(srcModule, testFolder, new HashSet<Module>());
+    }
+
+    private static void checkForTestRoots(Module srcModule, HashSet<VirtualFile> testFolder, HashSet<Module> processed) {
+        boolean isFirst = processed.isEmpty();
+        if (!processed.add(srcModule)) return;
+        testFolder.addAll(ModuleRootManager.getInstance(srcModule).getSourceRoots(JavaSourceRootType.TEST_SOURCE));
+        if (isFirst && !testFolder.isEmpty()) return;
+        HashSet<Module> modules = new HashSet<>();
+        ModuleUtilCore.collectModulesDependsOn(srcModule, modules);
+        for (Module module : modules) {
+            checkForTestRoots(module, testFolder, processed);
+        }
+    }
+
+    @Override
+    public boolean isAvailable(@NotNull Project project, Editor editor, @NotNull PsiElement element) {
+        if (!isAvailbleForElement(element)) return false;
+        PsiClass containingClass = getContainingClass(element);
+        assert containingClass != null;
+        PsiElement leftBrace = containingClass.getLBrace();
+        if (leftBrace == null) {
+            return false;
+        }
+        if (element.getTextOffset() >= leftBrace.getTextOffset()) return false;
+        return true;
+    }
+
+    @NotNull
+    @Override
+    public String getText() {
+        return GENERATE_DAOXML;
+    }
+
+    public static boolean isAvailbleForElement(PsiElement psiElement) {
+        if (psiElement == null) {
+            return false;
+        }
+
+        PsiClass containingClass = getContainingClass(psiElement);
+        if (containingClass == null) return false;
+        Module srcMoudle = ModuleUtilCore.findModuleForPsiElement(containingClass);
+        if (srcMoudle == null) return false;
+        if (containingClass.isAnnotationType() || containingClass instanceof PsiAnonymousClass) {
+            return false;
+        }
+        return true;
+    }
+
+
+    private static PsiClass getContainingClass(PsiElement psiElement) {
+        PsiClass psiClass = PsiTreeUtil.getParentOfType(psiElement, PsiClass.class, false);
+        if (psiClass == null) {
+            PsiFile containingFile = psiElement.getContainingFile();
+            if (containingFile instanceof PsiClassOwner) {
+                PsiClass[] classes = ((PsiClassOwner) containingFile).getClasses();
+                if (classes.length == 1) {
+                    return classes[0];
+                }
+            }
+        }
+        return psiClass;
+    }
+
+    @Nls
+    @NotNull
+    @Override
+    public String getFamilyName() {
+        return GENERATE_DAOXML;
+    }
+}
