4df02cd82a93eb3594d905e5b3885829ca0e8d15
gejun123456 <gejunxxx@163.com>
2016-12-11 15:47:53 +0800

    add exist dialog

---
+++ src/main/java/com/ccnode/codegenerator/dialog/MethodExistDialog.java
@@ -0,0 +1,47 @@
+package com.ccnode.codegenerator.dialog;
+
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.ui.DialogWrapper;
+import org.jetbrains.annotations.Nullable;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * Created by bruce.ge on 2016/12/11.
+ */
+public class MethodExistDialog extends DialogWrapper {
+
+    private final Project myProject;
+
+    private final String existMethodValue;
+
+    protected MethodExistDialog(@Nullable Project project, String existMethodValue) {
+        super(project, true);
+        myProject = project;
+        this.existMethodValue = existMethodValue;
+        init();
+    }
+
+
+    @Nullable
+    @Override
+    protected JComponent createCenterPanel() {
+        JPanel jpanel = new JPanel(new GridBagLayout());
+        GridBagConstraints bag = new GridBagConstraints();
+        bag.fill = GridBagConstraints.HORIZONTAL;
+        bag.anchor = GridBagConstraints.WEST;
+        bag.gridx = 0;
+        bag.gridy = 0;
+        jpanel.add(new JLabel("the method already exist in xml,the existed value is:"), bag);
+        bag.gridx = 0;
+        bag.gridy = 1;
+        jpanel.add(new JLabel(existMethodValue), bag);
+
+        bag.gridy = 2;
+        jpanel.add(new JButton("ovveride them"), bag);
+        bag.gridx = 1;
+        jpanel.add(new JButton("cancel"), bag);
+        return jpanel;
+    }
+}
