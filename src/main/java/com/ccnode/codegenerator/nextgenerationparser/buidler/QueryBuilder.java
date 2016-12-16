2127ee2eada5afbaf0fd37b63a63bd7625b42967
gejun123456 <gejunxxx@163.com>
2016-12-16 20:02:26 +0800

    fix with update prop.

--- src/main/java/com/ccnode/codegenerator/nextgenerationparser/buidler/QueryBuilder.java
+++ src/main/java/com/ccnode/codegenerator/nextgenerationparser/buidler/QueryBuilder.java
@@ -314,7 +314,7 @@ public class QueryBuilder {
             ParamInfo paramInfo = ParamInfo.ParamInfoBuilder.aParamInfo().withParamAnno("updated" + firstCharUpper(prop)).
                     withParamType(extractLast(fieldMap.get(prop))).withParamValue("updated" + firstCharUpper(prop)).build();
             info.getParamInfos().add(paramInfo);
-            builder.append(" " + GenCodeUtil.getUnderScoreWithComma(prop) + "={" + paramInfo.getParamAnno() + "}");
+            builder.append(" " + GenCodeUtil.getUnderScoreWithComma(prop) + "=#{" + paramInfo.getParamAnno() + "}");
             if (i != update.getUpdateProps().size() - 1) {
                 builder.append(",");
             }
